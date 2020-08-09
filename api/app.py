59bf352c6f40de8aa852874c0817ed70253fa307
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-08-09 13:33:24 +0530

    added api strucuture

---
+++ api/app.py
@@ -0,0 +1,34 @@
+import initialize
+
+from os import getenv
+
+from flask import Flask, request, make_response
+from flask_cors import CORS
+from time import strftime
+
+from utils import logger, send_resp
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['ENV'] = getenv("PYTHON_ENV")
+app.config['DEBUG'] = getenv("FLASK_DEBUG")
+app.config['TESTING'] = getenv("FLASK_TESTING")
+
+@app.after_request
+def after_request(response):
+  if response.status_code != 500:
+    ts = strftime('[%Y-%b-%d %H:%M]')
+    logger.info('%s %s %s %s %s %s',
+                ts,
+                request.remote_addr,
+                request.method,
+                request.scheme,
+                request.full_path,
+                response.status)
+  return response
+
+@app.errorhandler(Exception) 
+def handle_error(error):
+  logger.exception(error)
+  return make_response(send_resp(500, "Something went wrong with the server"), 500)
