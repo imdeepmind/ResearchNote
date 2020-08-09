27fab5300c8156546b8334dfd765f76ad19eed60
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-08-10 00:12:35 +0530

    completed sidebar

--- web/src/app/components/Sidebar/index.js
+++ web/src/app/components/Sidebar/index.js
@@ -1,41 +1,39 @@
 import React from "react";
 
 import { Layout, Menu } from "antd";
-import {
-  UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined,
-} from "@ant-design/icons";
+import { UserOutlined, CopyOutlined, PlusOutlined } from "@ant-design/icons";
 
 const { SubMenu } = Menu;
 const { Sider } = Layout;
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+  const { profile, notes } = props;
+
+  const { name, logout, deleteAcc } = profile;
+  const { newNote, allNotes, openNote } = notes;
+
   return (
     <Sider width={250} className="site-layout-background">
       <Menu
         mode="inline"
         defaultSelectedKeys={["1"]}
-        defaultOpenKeys={["sub1"]}
-        style={{ height: "100%", borderRight: 0 }}
+        defaultOpenKeys={["notes"]}
+        style={{
+          height: "100vh",
+          borderRight: 0,
+          overflow: "hidden",
+          overflowY: "scroll",
+        }}
       >
-        <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-          <Menu.Item key="1">option1</Menu.Item>
-          <Menu.Item key="2">option2</Menu.Item>
-          <Menu.Item key="3">option3</Menu.Item>
-          <Menu.Item key="4">option4</Menu.Item>
+        <Menu.Item onClick={newNote}><PlusOutlined />New Note</Menu.Item>
+        <SubMenu key="notes" icon={<CopyOutlined />} title="Notes">
+          {allNotes.map((val) => {
+            return <Menu.Item key={val.id} onClick={openNote}>{val.title}</Menu.Item>;
+          })}
         </SubMenu>
-        <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-          <Menu.Item key="5">option5</Menu.Item>
-          <Menu.Item key="6">option6</Menu.Item>
-          <Menu.Item key="7">option7</Menu.Item>
-          <Menu.Item key="8">option8</Menu.Item>
-        </SubMenu>
-        <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-          <Menu.Item key="9">option9</Menu.Item>
-          <Menu.Item key="10">option10</Menu.Item>
-          <Menu.Item key="11">option11</Menu.Item>
-          <Menu.Item key="12">option12</Menu.Item>
+        <SubMenu key="account" icon={<UserOutlined />} title={name}>
+          <Menu.Item onClick={logout}>Logout</Menu.Item>
+          <Menu.Item onClick={deleteAcc}>Delete Account</Menu.Item>
         </SubMenu>
       </Menu>
     </Sider>
--- web/src/app/containers/Notes/index.js
+++ web/src/app/containers/Notes/index.js
@@ -5,10 +5,42 @@ import Sidebar from "../../components/Sidebar";
 import { Layout } from "antd";
 
 const App = () => {
+  const deleteAcc = () => {
+    console.log("Delete Account");
+  };
+
+  const logout = () => {
+    console.log("Logout Account");
+  };
+
+  const newNote = () => {
+    console.log("New Note");
+  };
+
+  const openNote = ({ key }) => {
+    console.log("Opening page ", key);
+  };
+
   return (
     <Layout>
       <Layout>
-        <Sidebar />
+        <Sidebar
+          profile={{
+            name: "Abhishek Chatterjee",
+            logout,
+            deleteAcc,
+          }}
+          notes={{
+            newNote,
+            allNotes: [
+              {
+                title: "Test Note",
+                id: "note_id",
+              },
+            ],
+            openNote,
+          }}
+        />
         <Layout>
           <Editor />
         </Layout>
