493dd9d6340cecdb37534025578fbf1e6e2312c8
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-08-09 23:43:37 +0530

    added ant design layout

--- web/src/app/containers/Notes/index.js
+++ web/src/app/containers/Notes/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Editor from "../../components/Editor";
+import Sidebar from "../../components/Sidebar";
+
+import { Layout } from "antd";
 
 const App = () => {
   return (
-    <div>
-      <Editor />
-    </div>
-  )
+    <Layout>
+      <Layout>
+        <Sidebar />
+        <Layout>
+          <Editor />
+        </Layout>
+      </Layout>
+    </Layout>
+  );
 };
 
 export default App;
--- web/src/index.js
+++ web/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import App from "./app";
 
 import './index.css';
+import 'antd/dist/antd.css';
 import * as serviceWorker from './serviceWorker';
 
 require("dotenv").config();
