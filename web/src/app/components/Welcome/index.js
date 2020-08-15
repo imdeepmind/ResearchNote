c74a046cb201f297e629e04fc892ec2e2d072274
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-08-15 21:53:23 +0530

    added welcome note

--- web/src/app/components/Welcome/index.js
+++ web/src/app/components/Welcome/index.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { Typography } from "antd";
+
+const { Title, Text } = Typography;
 
 const Welcome = () => {
   return (
-    <div>
-      <h1>Hello, welcome to Research Notes</h1>
-      <p>Ultra minimalistic open source note taking app</p>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <Title>Hello, welcome to Research Notes</Title>
+      <Text>
+        Research Notes is a ultra minimalistic open source note taking app.
+        Click on the "New Note" button to create a new note.
+      </Text>
     </div>
-  )
-}
+  );
+};
 
 export default Welcome;
